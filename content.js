260ded45de9ee2e1574c024df2074ddb32e32be2
weveree <wahil.boulahbach.blog@gmail.com>
2025-08-16 18:57:35 +0200

    💄 css
    changed layout checkboxes and texts

--- content.js
+++ content.js
@@ -3,12 +3,14 @@ let checkbox = []
 let to_dl = []
 let downloading = false
 
+//Add download button
 let dl = document.createElement("p")
 dl.style.textDecoration = 'underline'
 dl.textContent = "Télécharger"
 dl.style.cursor = "pointer"
 document.querySelector(".documents > thead > tr:first-of-type th").appendChild(dl);
 
+//Add All dl checkbox
 let master_checkbox_parent = document.createElement("div")
 let master_checkbox = document.createElement("input")
 master_checkbox.type = "checkbox"
@@ -20,15 +22,16 @@ master_checkbox.addEventListener("click", (e) => {
 let master_checkbox_label = document.createElement("label")
 master_checkbox_label.innerText = "Tout télécharger ?"
 
-master_checkbox_parent.appendChild(master_checkbox_label)
 master_checkbox_parent.appendChild(master_checkbox)
+master_checkbox_parent.appendChild(master_checkbox_label)
 document.querySelector(".documents > thead > tr:first-of-type th").appendChild(master_checkbox_parent);
 
 
+//Add checkbox col
 let tr_ = document.createElement("tr");
-tr_.textContent = "dl ?";
+tr_.textContent = "Choix";
 tr_.style.display = "table-cell";
-document.querySelector(".documents thead tr+tr").appendChild(tr_);
+document.querySelector(".documents thead tr+tr").insertBefore(tr_,document.querySelector(".documents thead tr+tr th:first-of-type"));
 
 
 document.querySelectorAll(".documents tbody tr").forEach(el=>{
@@ -37,9 +40,13 @@ document.querySelectorAll(".documents tbody tr").forEach(el=>{
     let check = document.createElement('input')
     check.type = "checkbox"
     td.appendChild(check)
-    el.appendChild(td)
+    el.children[0].appendChild(td)
+    el.children[0].style.display = "flex"
+    el.children[0].style.flexDirection = "row-reverse"
+
     checkbox.push([check,el.querySelector("td:first-of-type+td a")])
 })
+
 function setDownloading(value)
 {
     dl.textContent= value ? "En cours de téléchargement ..." :"Télécharger"
